2b2e5f41fa5c16800a9cb0cc50161adb491d3c1b
Jatin5233 <70570803+Jatin5233@users.noreply.github.com>
2020-10-29 13:57:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var gameState=0;
+var playerCount;
+var form,player,game;
+var database;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+  game=new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+    background("white");
+
+}
+
